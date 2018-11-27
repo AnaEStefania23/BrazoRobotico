b4a8b9f6c15411e038e95748d1260c1938b02635
AnaEStefania23 <43787566+AnaEStefania23@users.noreply.github.com>
2018-11-27 11:13:39 -0600

    Update Brazo.java

--- Brazo.java
+++ Brazo.java
@@ -1,10 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Práctica Brazo robotico, este archivo muestra el código de la programación de el brazo, donde se pod4rá distinguir los 
+servo motores, el motor a pasos controlados por medio de esta interfaz, teniendo en cuenta una conexión Java-Arduino.
+hecho por: Ana Estefania Mejia Rocha,   Crhistian josue
  */
 package brazo;
-
+//importación de libreiras, estas librerias las puedes encontrar en la caja de archivos de este proyecto
 import com.panamahitek.ArduinoException;
 import com.panamahitek.PanamaHitek_Arduino;
 import java.awt.Container;
@@ -17,10 +17,10 @@ import java.util.logging.Logger;
 import jssc.SerialPortException;
 
 public class Brazo extends JFrame {
-
-    JButton Izquierda;
-    JButton Derecha;
-    JButton Arriba;
+//Secrean los elementos de la interfaz, botones, panel, y label
+    JButton Izquierda; //boton que girará al motor a pasos a la izquierda
+    JButton Derecha; //boton que girará al motor a pasos a la derecha
+    JButton Arriba; 
     JButton Abajo;
     JButton Abrir;
     JButton Cerrar;
@@ -31,9 +31,12 @@ public class Brazo extends JFrame {
     JPanel panel1, panel2, panel3;
     JLabel f;
     ImageIcon image;
-    private final String PORT_NAME = "/dev/ttyACM0";
+    
+    //se crea los datos para la conexión a el programa Arduino
+    private final String PORT_NAME = "/dev/ttyACM0"; //puerto linux, aquí cambiar a tu puerto de entrada al arduino.
     private static final int TIME_OUT = 2000;
     private static final int DATA_RATE = 9600;
+    //inicialización en ceros de los movimientos, esto para que empiece de una forma limpia
     String a = "0";
     String b = "0";
     String c = "0";
@@ -42,20 +45,22 @@ public class Brazo extends JFrame {
     String j = "0";
     String g = "0";
     String h = "0";
-    // mensaje="";
-
+   
     //Creamos una instancia de la clase PanamaHitek_Arduino para iniciar la conexion
     PanamaHitek_Arduino ino = new PanamaHitek_Arduino();
 
+    
+   
     public Brazo() {
         super("Brazo Robotico");
         //setVisible(true);
         setSize(9000,450);//tamaño de la venta(tamaño y altura)
         setLayout(null);//propiedades para ubicar en posicion
        
-      image = new ImageIcon(getClass().getResource("/imagen/fondo4.jpg"));
+      image = new ImageIcon(getClass().getResource("/imagen/fondo4.jpg")); //imnagen de fondo de la intefaz
         f = new JLabel(image);
          
+       //crear los elementos
         Izquierda = new JButton();
         Derecha = new JButton();
         Arriba = new JButton();
@@ -65,7 +70,7 @@ public class Brazo extends JFrame {
         Abrir = new JButton();
         Cerrar = new JButton();
         Ejecutar = new JButton();
-
+            //añadir los elemntos 
         f.setBounds(0, 0, 000, 350);
         add(f);
         f.add(Izquierda);
@@ -82,7 +87,7 @@ public class Brazo extends JFrame {
 
         Izquierda.setText("Izquierda");
         Izquierda.setBounds(190, 120, 90, 30);
-
+        //Posición de los elemntos en la intefaz
         Derecha.setText("Derecha");
         //30, 70, 90, 30
         Derecha.setBounds(340, 120, 90, 30);
@@ -109,12 +114,12 @@ public class Brazo extends JFrame {
         Cerrar.setBounds(400, 250, 90, 30);
 
         try {
-            //Iniciamos la conexion a arduino por medio del puerto COM17
+            //Iniciamos la conexion a arduino por medio del puerto 
             ino.arduinoTX(PORT_NAME, DATA_RATE);
         } catch (ArduinoException ex) {
             Logger.getLogger(Brazo.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+            
      
           Registro_m manejador = new Registro_m();
        
@@ -128,36 +133,37 @@ public class Brazo extends JFrame {
          Cerrar.addActionListener(manejador);
          Derecha.addActionListener(manejador);
     }
-
+        //eventos
      private class Registro_m  implements ActionListener{
          @Override
         public void actionPerformed(ActionEvent e) {
-           
+           //si se presiona 1, se manda al boton arriba
         
         if (e.getSource() == Arriba) {
             a = "1";
         }
+              //si se presiona 2se manda al boton regresar
         if (e.getSource() == Regresar) {
             b = "2";
-        }
+        }  //si se presiona 3 se manda al boton abrir
         if (e.getSource() == Abrir) {
             c = "3";
-        }
+        }  //si se presiona 4 se manda al boton Izquierda
         if (e.getSource() == Izquierda) {
             d = "4";
-        }
+        }  //si se presiona 5 se manda al boton abajo
         if (e.getSource() == Abajo) {
             g = "5";
-        }
+        }  //si se presiona 6 se manda al boton avanzar
         if (e.getSource() == Avanzar) {
             h = "6";
-        }
+        }  //si se presiona 7 se manda al boton cerrar
         if (e.getSource() == Cerrar) {
             i = "7";
-        }
+        }  //si se presiona 8 se manda al boton derecha
         if (e.getSource() == Derecha) {
             j = "8";
-        }
+        }  //si se presiona ejecutar, guarda y envia los datos para el modo automatico
         if (e.getSource() == Ejecutar) {
             EnviarDatos("9");
             EnviarDatos(a);
@@ -186,7 +192,7 @@ public class Brazo extends JFrame {
             Logger.getLogger(Brazo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+//MAIN
     public static void main(String[] args) {
         Brazo a = new Brazo();
         a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
